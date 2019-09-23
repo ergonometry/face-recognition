0deab8f0351963551982fc16e2d96665555a75f4
ergonometry <hoangkevinh@gmail.com>
2019-09-23 04:49:55 -0400

    Updates

---
+++ precache-manifest.28d61daddc4cd16d93cda849f47c2601.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "9a6a6a0c42958dd8e526",
+    "url": "/facerecognition/static/js/runtime~main.7f9ce1ea.js"
+  },
+  {
+    "revision": "9ba5a831503e4902917b",
+    "url": "/facerecognition/static/js/main.a2731936.chunk.js"
+  },
+  {
+    "revision": "8b1e1a225b46440b7c1b",
+    "url": "/facerecognition/static/js/2.1feacca2.chunk.js"
+  },
+  {
+    "revision": "9ba5a831503e4902917b",
+    "url": "/facerecognition/static/css/main.af22e2e4.chunk.css"
+  },
+  {
+    "revision": "8b1e1a225b46440b7c1b",
+    "url": "/facerecognition/static/css/2.3f59ffd3.chunk.css"
+  },
+  {
+    "revision": "e0ee410dd72e44741f4050d998cff2b0",
+    "url": "/facerecognition/index.html"
+  }
+];
